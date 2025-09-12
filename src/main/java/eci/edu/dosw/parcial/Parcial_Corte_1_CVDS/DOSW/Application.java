c458e4e0694961009f1a97bef733c39d12280c4b
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2025-09-12 09:06:41 -0500

    dejar pom listo

---
+++ src/main/java/eci/edu/dosw/parcial/Parcial_Corte_1_CVDS/DOSW/Application.java
@@ -0,0 +1,10 @@
+package eci.edu.dosw.parcial.Parcial_Corte_1_CVDS.DOSW;
+
+
+public class Application {
+
+	public static void main(String[] args) {
+        System.out.println("Aplicación iniciada correctamente");
+	}
+
+}
---
+++ src/test/java/eci/edu/dosw/parcial/Parcial_Corte_1_CVDS/DOSW/ApplicationTests.java
@@ -0,0 +1,12 @@
+package eci.edu.dosw.parcial.Parcial_Corte_1_CVDS.DOSW;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
